a5caa4a4b26ef728130f4d0805fc6513607e3e29
LHB-3242 <bm7852616@gmail.com>
2026-04-21 13:50:43 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import StateSecurity from "./pages/StateSecurity";
 import StateProtocols from "./pages/StateProtocols";
@@ -9,14 +9,14 @@ import AdminPanel from "./pages/AdminPanel";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/state-security-laws" element={<StateSecurity />} />
           <Route path="/state-security-protocols" element={<StateProtocols />} />
           <Route path="/admin-vienna-rp" element={<AdminPanel />} />
         </Routes>
-      </BrowserRouter>
+      <Router>
     </div>
   );
 }
